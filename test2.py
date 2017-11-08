b22f3994f1d6d6e00f5a2ca71b57d002f4105de2
Rushat Gabhane <rushatgabhane@gmail.com>
2017-11-09 03:48:30 +0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,4 +1,9 @@
+#initializes a set which gets words from words_alpha
 words = set(open('words_alpha.txt').read().split())
+
+#another set which gets words only if lenght>1 and the remaining letters form a word that belongs to words_alpha
 subwords = {s: s[1:] for s in words if len(s) > 1 and s[1:] in words}
+
+#outputs subwords to a file eyes.txt
 with open('eyes.txt', 'w') as f:
     print(subwords, file=f)
